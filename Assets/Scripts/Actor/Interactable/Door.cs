26b2eb3a74f362428825172c6c9db127d5e8f412
Aru Liao <gary1148@gmail.com>
2024-01-30 12:16:52 +0800

    Add bool in Door script

--- Assets/Scripts/Actor/Interactable/Door.cs
+++ Assets/Scripts/Actor/Interactable/Door.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Door : Actor
 {
-    public override bool IsBlocked(Vector2 movingDir){ return true; }
+    public override bool IsBlocked(Vector2 movingDir){ return !IsOpen; }
+    public bool IsOpen = false;
     public void OpenDoor()
     {
         print("OpenDoor!");
+        IsOpen = true;
         gameObject.SetActive(false);
     }
     public void CloseDoor()
     {
         print("CloseDoor!");
+        IsOpen = false;
         gameObject.SetActive(true);
     }
 }
